943064485cb92ac19c75f269b3f7e3e377b4dabb
l-martinetti <luca.martinetti@outlook.it>
2025-02-05 17:19:56 +0100

    feat: add index query connection
    -Milestone 2 complete

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,13 @@
 const connection = require('../data/db');
 
 const index = (req, res) => {
-    res.send('Elenco post')
+    const sql = 'SELECT * FROM posts';
+
+    connection.query(sql, (err, results) => {
+        if (err) res.status(500).json({ error: 'Query al database fallita' })
+
+        res.json(results)
+    })
 };
 
 const show = (req, res) => {
